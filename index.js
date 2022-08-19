5293515b4e2f841d5f1b1b246b4100a293553223
KemiDoyin <kemidoy@gmail.com>
2022-08-19 11:17:19 +0100

    declare variables

---
+++ index.js
@@ -0,0 +1,6 @@
+const bill = document.getElementById('bill');
+const tip = document.getElementById('tip');
+const totalPersons = document.getElementById('total_persons');
+const tipAmount = document.querySelector('.tip_amount');
+const totalAmount = document.querySelector('.total_amount');
+const tipButton = document.querySelectorAll('.tip_button');
