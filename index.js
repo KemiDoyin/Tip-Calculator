ecfa9a7af0b47b14756c185b72d8d9cb2fa37d69
KemiDoyin <kemidoy@gmail.com>
2022-08-20 12:30:11 +0100

    adding functions

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const totalPersons = document.getElementById('total_persons');
 const tipAmount = document.querySelector('.tip_amount');
 const totalAmount = document.querySelector('.total_amount');
 const tipButton = document.querySelectorAll('.tip_button');
-const errors = document.querySelector('.error')
+const errors = document.querySelectorAll('.error')
+const inputS = document.querySelectorAll('.inputs_')
 const totalPersonError = document.querySelector('.total_person_error')
 const billError = document.querySelector('.bill_error')
 const tipError = document.querySelector('.tip_error')
@@ -13,12 +14,14 @@ const warning = document.querySelector('.warning')
 // const inputNum = document.getElementsByTagName(');
 
 // const regExes = {
-//     'dot': /^\.$/g,
+//     dot: /^\.$/g,
 //     zero: /^0\d+/g,
 //     num: /[^0-9\.]+/g,
 //     people: /[^0-9]+/g,
 //     letter: /[a-zA-Z,/<>\?;':""[\]\\{}\|`~!@#\$%\^&\*()_=\+]+/g,
 //   };
+
+//   const testExp = (reg, val) => new RegExp(reg).test(val)
 
 // function showError() {
 //     if (totalPersons.value < 1 || bill.value < 1 ) {
@@ -62,25 +65,77 @@ const warning = document.querySelector('.warning')
 //         warning.classList.remove('error')
 //     }
 // }
-function showError() {
-    if (bill.value === '0') {
-        billError.textContent = 'Can"t be zero'
-        bill.classList.add('error')
-    }
-    else if (isNaN(bill.value)) {
-        billError.textContent = 'Invalid'
-        bill.classList.add('error')
-    }
+const displayError = function(e, msg) {
+    e.target.classList.add('error')
+  e.target.previousElementSibling.lastElementChild.textContent = msg;
 }
-totalPersons.addEventListener('change',(e) => {
-    // showError();
-   
+
+const removeError = function (e) {
+    e.target.classList.remove('error')
+    e.target.previousElementSibling.lastElementChild.textContent = '';
+}
+
+inputS.forEach(inputs => {
+    inputs.addEventListener('input', (e) => {
+        const value = e.target.value;
+        if (value === '0') {
+            displayError(e, "Can't be zero")
+        }
+        else if (isNaN(value)) {
+            displayError(e, 'invalid')
+        }
+        else if (value < 0) {
+            displayError(e, 'positive only')
+        }
+        else if (value.includes('.') && e.target.name === 'total_persons') {
+            displayError(e, 'error')
+        }
+        else {
+            removeError(e)
+        }
+        display()
+    })
 })
-bill.addEventListener('input',(e) => {
-    showError();
+
+const display = () => {
+    const billInput = bill.value;
+    const tipInput = tip.value;
+    const totalPeople = totalPersons.value;
+    const totalPerPerson = billInput + totalPeople
+    const tipAmountPerPerson = '';
+    totalAmount.textContent = totalPerPerson;
+}
+
+// const input = function(e) {
+//     const value = e.target.value;
+//     if (value === '0') {
+//         errors.textContent = "cant be zero";
+//     }
+//     else if (value > 10 && e.target.name === 'bill') {
+//         billError.textContent = 'lol'
+//     }
+// }
+// function showError() {
+//     if (bill.value === '0') {
+//         billError.textContent = 'Can"t be zero'
+//         bill.classList.add('error')
+//     }
+//     else if (isNaN(bill.value)) {
+//         billError.textContent = 'Invalid'
+//         bill.classList.add('error')
+//     }
+// }
+// totalPersons.addEventListener('input',(e) => {
+//     // showError();
+//    input(e)
+// })
+// bill.addEventListener('input',(e) => 
+//     // showError();
+//     // input(e)
     
 
-})
-tip.addEventListener('change',() => {
-    showError();
-})
+// )
+// tip.addEventListener('input',(e) => {
+//     // showError();
+//     input(e);
+// })
